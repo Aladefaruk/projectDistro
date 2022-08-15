623ea2d64e0dd873b9006a29135a9f3c3e9d6d72
Alade Faruk <alade.faruk11@gmail.com>
2022-08-15 13:57:54 +0100

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,124 +59,7 @@ endDateofCurrenProject:"2022-09-02T19:34:25.663Z"
 }
 ]
 
-width: number = 100;
-height: number = 100;
-myStyle: Object = {
-  'position': 'fixed',
-  'width': '100%',
-  'height': '100%',
-  'z-index': -1,
-  'top': 0,
-  'left': 0,
-  'right': 0,
-  'bottom': 0,
-};
-myParams: object = {
-  "particles": {
-    "number": {
-      "value": 80,
-      "density": {
-        "enable": true,
-        "value_area": 800
-      }
-    },
-    "color": {
-      "value": "#010B30"
-    },
-    "shape": {
-      "type": "circle",
-      "polygon": {
-        "nb_sides": 7
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.6,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 3,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 150,
-      "color": "#5B566E",
-      "opacity": 0.4,
-      "width": 1.5
-    },
-    "move": {
-      "enable": true,
-      "speed": 4,
-      "direction": "none",
-      "random": false,
-      "straight": false,
-      "out_mode": "out",
-      "bounce": false,
-      "attract": {
-        "enable": false,
-        "rotateX": 600,
-        "rotateY": 1200
-      }
-    }
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": false,
-        "mode": "repulse"
-      },
-      "onclick": {
-        "enable": false,
-        "mode": "push"
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 400,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
-    }
-  },
-  "retina_detect": true
-};
+
   
   constructor( public testService: TestService){
    
@@ -237,7 +120,7 @@ myParams: object = {
           this.errorMsg="Input the Start Date of your project"
         }
         if(i.endDateofCurrenProject===undefined){
-          if(i.role==="UI"){
+          if(i.role=="UI"){
             this.uiTeam.push(i)
           }else if(i.role=="BE"){
             this.beTeam.push(i)
@@ -247,7 +130,7 @@ myParams: object = {
 
           }
         }else if(((convertedEndDate <= this.startDate)||(convertedStartDate>= this.endDate))){
-          if(i.role==="UI"){
+          if(i.role=="UI"){
             this.uiTeam.push(i)
           }else if(i.role=="BE"){
             this.beTeam.push(i)
@@ -256,37 +139,32 @@ myParams: object = {
             this.feTeam.push(i)
           }
         }
+       
       }
       
 
-      if(this.feTeam.length<=Number(this.FE)){
+      if(this.feTeam.length<Number(this.FE)){
         this.newFE= Number(this.FE)- this.feTeam.length
       }else{
-        this.feTeam= this.feTeam.splice(Number(this.FE))
+        this.feTeam= this.feTeam.slice(0,Number(this.FE))
       }
 
       if(this.beTeam.length<Number(this.BE)){
         this.newBE= Number(this.BE)- this.beTeam.length 
       }else{
-        this.beTeam= this.beTeam.splice(Number(this.FE))
+                this.beTeam= this.beTeam.slice(0,Number(this.BE))
       }
       if(this.uiTeam.length<Number(this.UI)){
         this.newUI= Number(this.UI)- this.uiTeam.length
-      
       } else{
-        this.uiTeam= this.uiTeam.splice(Number(this.FE))
+        this.uiTeam= this.uiTeam.slice(0,Number(this.UI))
       }
      
     console.log(this.uiTeam,this.beTeam,this.feTeam)
     console.log(this.newUI,this.newBE,this.newFE) 
   }
   
-  clearData(){
-      this.uiTeam=[]
-      this.beTeam=[]
-      this.feTeam=[]
-      
-  }
+ 
   // addMember() {
   //   if(!this.newMember){
   //     this.errorMsg="Name cannot be empty"
